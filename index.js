cf05459b787557e38bfdb42885b0cb8924042d2f
raychua <raychua@users.noreply.github.com>
2020-04-30 13:44:44 +0800

    push to prod

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require("./playlist");
 const env = require("./env/env.js");
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Playlist API running on http://localhost:${process.env.PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Playlist API running on http://localhost:${PORT}`);
 });
